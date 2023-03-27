18a955f01c5ceca7ed5e59161eb5c62655239abe
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-03-27 19:17:07 +0600

    skill add

--- lib/Components/side_bar.dart
+++ lib/Components/side_bar.dart
@@ -15,23 +15,58 @@ class NaveSideBar extends StatelessWidget {
         children: [
           const MyInfo(),
           Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(defaultPadding),
-            child: Column(
-              children: const [
-                BasicInfoText(
-                  title: 'Residence',
-                  text: 'Bangladesh',
-                ),
-                BasicInfoText(
-                  title: 'City',
-                  text: 'Dhaka',
-                ),
-                BasicInfoText(
-                  title: 'Age',
-                  text: '22',
-                ),
-              ],
+              child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(defaultPadding),
+              child: Column(
+                children: [
+                  const BasicInfoText(
+                    title: 'Residence',
+                    text: 'Bangladesh',
+                  ),
+                  const BasicInfoText(
+                    title: 'City',
+                    text: 'Dhaka',
+                  ),
+                  const BasicInfoText(
+                    title: 'Age',
+                    text: '22',
+                  ),
+                  const Divider(),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: defaultPadding),
+                    child: Text(
+                      "Skills",
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 1,
+                        child: TweenAnimationBuilder(
+                            tween: Tween<double>(begin: 0, end: 1),
+                            duration: defaultDuration,
+                            builder: (context, double value, child) =>
+                                Stack(
+                                  fit: StackFit.expand,
+                                  children: [
+                                    CircularProgressIndicator(
+                                      value: value,
+                                      color: primaryColor,
+                                      backgroundColor: darkColor,
+                                    ),
+                                    Center(child: Text("${value*100}%", style: Theme.of(context).textTheme.titleMedium,))
+                                  ],
+                                )),
+                      ),
+                      const SizedBox(height: defaultPadding/2,),
+                      Text('Flutter', style: Theme.of(context).textTheme.titleSmall,)
+                    ],
+                  )
+                ],
+              ),
             ),
           ))
         ],
