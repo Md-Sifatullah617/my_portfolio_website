a6e867bcdd782dba45093333152eab2c8c2591c3
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-04-01 22:47:30 +0600

    kjdf

--- lib/Components/my_info.dart
+++ lib/Components/my_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../constents.dart';
 
@@ -16,16 +17,26 @@ class MyInfo extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            const Spacer(flex: 2,),
+            const Spacer(
+              flex: 2,
+            ),
             const CircleAvatar(
               radius: 50,
-              backgroundImage: AssetImage("/images/pp.jpg"),
+              backgroundImage: AssetImage("assets/images/pp.jpg"),
             ),
             const Spacer(),
-            Text('Md. Sifatullah', style: Theme.of(context).textTheme.titleSmall,),
-            const Text('Flutter Developer & Founder of\nesuIT', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w200, height: 1.5),
+            Text(
+              'Md. Sifatullah',
+              style: Theme.of(context).textTheme.titleSmall,
             ),
-            const Spacer(flex: 2,),
+            const Text(
+              'Flutter Developer & Founder of\nesuIT',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.w200, height: 1.5),
+            ),
+            const Spacer(
+              flex: 2,
+            ),
           ],
         ),
       ),
@@ -35,7 +46,9 @@ class MyInfo extends StatelessWidget {
 
 class AnimatedCircularSkill extends StatelessWidget {
   const AnimatedCircularSkill({
-    super.key, required this.label, required this.percentage,
+    super.key,
+    required this.label,
+    required this.percentage,
   });
   final String label;
   final double percentage;
@@ -49,22 +62,20 @@ class AnimatedCircularSkill extends StatelessWidget {
               tween: Tween<double>(begin: 0, end: percentage),
               duration: defaultDuration,
               builder: (context, double value, child) => Stack(
-                fit: StackFit.expand,
-                children: [
-                  CircularProgressIndicator(
-                    value: value,
-                    color: primaryColor,
-                    backgroundColor: darkColor,
-                  ),
-                  Center(
-                      child: Text(
-                          "${(value*100).toInt()}%",
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium,
+                    fit: StackFit.expand,
+                    children: [
+                      CircularProgressIndicator(
+                        value: value,
+                        color: primaryColor,
+                        backgroundColor: darkColor,
+                      ),
+                      Center(
+                          child: Text(
+                        "${(value * 100).toInt()}%",
+                        style: Theme.of(context).textTheme.titleMedium,
                       ))
-                ],
-              )),
+                    ],
+                  )),
         ),
         const SizedBox(
           height: defaultPadding / 2,
@@ -76,6 +87,156 @@ class AnimatedCircularSkill extends StatelessWidget {
           style: Theme.of(context).textTheme.titleSmall,
         )
       ],
+    );
+  }
+}
+
+class AnimatedCodingSkill extends StatelessWidget {
+  const AnimatedCodingSkill({
+    super.key,
+    required this.codingSkill,
+    required this.percentage,
+  });
+  final String codingSkill;
+  final double percentage;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: defaultPadding),
+      child: TweenAnimationBuilder(
+          tween: Tween<double>(begin: 0, end: percentage),
+          duration: defaultDuration,
+          builder: (context, double value, child) => Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        codingSkill,
+                        style: const TextStyle(color: colorWhite),
+                      ),
+                      Text('${(value * 100).toInt()}%')
+                    ],
+                  ),
+                  const SizedBox(
+                    height: defaultPadding / 2,
+                  ),
+                  LinearProgressIndicator(
+                    value: value,
+                    color: primaryColor,
+                    backgroundColor: darkColor,
+                  ),
+                ],
+              )),
+    );
+  }
+}
+
+class Skills extends StatelessWidget {
+  const Skills({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: Text(
+            "Skills",
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ),
+        Row(
+          children: const [
+            Expanded(
+                child: AnimatedCircularSkill(
+              label: 'Flutter',
+              percentage: 0.8,
+            )),
+            SizedBox(
+              width: defaultPadding,
+            ),
+            Expanded(
+                child: AnimatedCircularSkill(
+              label: 'Firebase',
+              percentage: 0.75,
+            )),
+            SizedBox(
+              width: defaultPadding,
+            ),
+            Expanded(
+                child: AnimatedCircularSkill(
+              label: 'API',
+              percentage: 0.6,
+            )),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class Knowledge extends StatelessWidget {
+  const Knowledge({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: Text(
+            'Knowledge\'s',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ),
+        const DetailsKNDG(
+          text: 'Flutter, Dart',
+        ),
+        const DetailsKNDG(
+          text: 'Git Knowledge',
+        ),
+        const DetailsKNDG(
+          text: 'Web Development',
+        ),
+        const DetailsKNDG(
+          text: 'Video Editing',
+        ),
+        const DetailsKNDG(
+          text: 'Programming',
+        ),
+      ],
+    );
+  }
+}
+
+class DetailsKNDG extends StatelessWidget {
+  const DetailsKNDG({
+    super.key,
+    required this.text,
+  });
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: defaultPadding / 2),
+      child: Row(
+        children: [
+          SvgPicture.asset('assets/icons/check.svg'),
+          const SizedBox(
+            width: defaultPadding / 2,
+          ),
+          Text(text)
+        ],
+      ),
     );
   }
 }
--- lib/Components/side_bar.dart
+++ lib/Components/side_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_portfolio_website/constents.dart';
 
 import 'my_info.dart';
@@ -19,7 +20,7 @@ class NaveSideBar extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(defaultPadding),
               child: Column(
-                children: [
+                children:   [
                   const BasicInfoText(
                     title: 'Residence',
                     text: 'Bangladesh',
@@ -37,6 +38,31 @@ class NaveSideBar extends StatelessWidget {
                     height: defaultPadding,
                   ),
                   const Coding(),
+                  const Knowledge(),
+                  const Divider(),
+                  const SizedBox(height: defaultPadding/2,),
+                  TextButton(onPressed: (){}, child: FittedBox(
+                    child: Row(
+                      children: [
+                        Text('Download CV', style: TextStyle(color: Theme.of(context).textTheme.bodyLarge!.color),),
+                        const SizedBox(width: defaultPadding/2,),
+                        SvgPicture.asset("assets/icons/download.svg"),
+                      ],
+                    ),
+                  )),
+                  Container(
+                    margin: const EdgeInsets.only(top: defaultPadding),
+                    color: const Color(0xff24242e),
+                    child: Row(
+                      children: [
+                        const Spacer(),
+                        IconButton(onPressed: (){}, icon: SvgPicture.asset('assets/icons/linkedin.svg')),
+                        IconButton(onPressed: (){}, icon: SvgPicture.asset('assets/icons/github.svg')),
+                        IconButton(onPressed: (){}, icon: SvgPicture.asset('assets/icons/twitter.svg')),
+                        const Spacer(),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
@@ -65,92 +91,25 @@ class Coding extends StatelessWidget {
             style: Theme.of(context).textTheme.titleSmall,
           ),
         ),
-        const AnimatedCodingSkill(codingSkill: 'Dart', percentage: 0.8,),
-        const AnimatedCodingSkill(codingSkill: 'C/C++', percentage: 0.75,),
-        const AnimatedCodingSkill(codingSkill: 'Python', percentage: 0.55,),
-        const AnimatedCodingSkill(codingSkill: 'HTML', percentage: 0.9,),
-        const AnimatedCodingSkill(codingSkill: 'CSS', percentage: 0.7,)
-      ],
-    );
-  }
-}
-
-class AnimatedCodingSkill extends StatelessWidget {
-  const AnimatedCodingSkill({
-    super.key, required this.codingSkill, required this.percentage,
-  });
-  final String codingSkill;
-  final double percentage;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: defaultPadding),
-      child: TweenAnimationBuilder(
-          tween: Tween<double>(begin: 0, end: percentage),
-          duration: defaultDuration,
-          builder: (context, double value, child) => Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(codingSkill, style: const TextStyle(color: colorWhite),),
-                  Text('${(value*100).toInt()}%')
-                ],
-              ),
-              const SizedBox(height: defaultPadding/2,),
-              LinearProgressIndicator(
-                    value: value,
-                    color: primaryColor,
-                    backgroundColor: darkColor,
-                  ),
-            ],
-          )),
-    );
-  }
-}
-
-class Skills extends StatelessWidget {
-  const Skills({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Divider(),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-          child: Text(
-            "Skills",
-            style: Theme.of(context).textTheme.titleSmall,
-          ),
+        const AnimatedCodingSkill(
+          codingSkill: 'Dart',
+          percentage: 0.8,
         ),
-        Row(
-          children: const [
-            Expanded(
-                child: AnimatedCircularSkill(
-              label: 'Flutter',
-              percentage: 0.8,
-            )),
-            SizedBox(
-              width: defaultPadding,
-            ),
-            Expanded(
-                child: AnimatedCircularSkill(
-              label: 'Firebase',
-              percentage: 0.75,
-            )),
-            SizedBox(
-              width: defaultPadding,
-            ),
-            Expanded(
-                child: AnimatedCircularSkill(
-              label: 'API',
-              percentage: 0.6,
-            )),
-          ],
+        const AnimatedCodingSkill(
+          codingSkill: 'C/C++',
+          percentage: 0.75,
+        ),
+        const AnimatedCodingSkill(
+          codingSkill: 'Python',
+          percentage: 0.55,
+        ),
+        const AnimatedCodingSkill(
+          codingSkill: 'HTML',
+          percentage: 0.9,
+        ),
+        const AnimatedCodingSkill(
+          codingSkill: 'CSS',
+          percentage: 0.7,
         )
       ],
     );
