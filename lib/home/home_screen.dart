feb49ed7227b6c40c05ee87d15365b566fd10c2e
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-04-03 14:59:10 +0600

    vbb

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -28,21 +28,42 @@ class HomeScreen extends StatelessWidget {
           shrinkWrap: true,
           itemCount: projectDetails.length,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-          ),
-          itemBuilder: (context, index) => Container(
-            padding: const EdgeInsets.all(defaultPadding),
-                color: secondaryColor,
-                child: Column(
-                    children: [
-                        Text(projectDetails[0].title!, 
-                        maxLines: 2,
-                        style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                    
-                    ],
-                ),
-              ))
+              crossAxisCount: 3,
+              mainAxisSpacing: defaultPadding,
+              crossAxisSpacing: defaultPadding,
+              childAspectRatio: 1.3),
+          itemBuilder: (context, index) => ProjectCard(project: projectDetails[index],))
     ]);
+  }
+}
+
+class ProjectCard extends StatelessWidget {
+  const ProjectCard({
+    super.key, required this.project,
+  });
+  final Projects project;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(defaultPadding),
+      color: secondaryColor,
+      child: Column(
+        children: [
+          Text(
+            project.title!,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          const SizedBox(height: defaultPadding,),
+          Text(
+            project.description!,
+            style: const TextStyle(height: 1.5),
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/projects_details.dart
+++ lib/projects_details.dart
@@ -7,7 +7,22 @@ class Projects {
 List<Projects> projectDetails = [
     Projects(
         title: "Covid 19 tracker app - with flutter UI",
+        description: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus. It provides the following features:\nReal-time updates on the latest cases and statistics\nAccess to resources and information on prevention and treatment\nAbility to set reminders and notifications for important news and updates\nInteractive maps to track the spread of the virus\nAbility to connect with others to share experiences and support"
+    ),
+    Projects(
+        title: "Covid 19 tracker app - with flutter UI",
         description: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus."
     ),
-    
+    Projects(
+        title: "Covid 19 tracker app - with flutter UI",
+        description: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus."
+    ),
+    Projects(
+        title: "Covid 19 tracker app - with flutter UI",
+        description: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus."
+    ),
+    Projects(
+        title: "Covid 19 tracker app - with flutter UI",
+        description: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus."
+    )
 ];
