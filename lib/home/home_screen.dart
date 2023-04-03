8cb6973249470a2c4acae2fc3c8e68665c92e441
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-04-04 01:25:42 +0600

    okj

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio_website/constents.dart';
 import 'package:my_portfolio_website/home/homepage.dart';
+import 'package:my_portfolio_website/recomandation.dart';
 import '../Components/banner.dart';
 import '../Components/highlighted_text.dart';
 import '../Components/projects.dart';
@@ -10,7 +11,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MyHomePage(children: [
+    return MyHomePage(children: [
       HOmeBanner(),
       SizedBox(
         height: defaultPadding,
@@ -19,8 +20,62 @@ class HomeScreen extends StatelessWidget {
       SizedBox(
         height: defaultPadding,
       ),
-      SectionProject()
+      SectionProject(),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Recommendation',
+                style: Theme.of(context).textTheme.titleLarge),
+            SizedBox(
+              height: defaultPadding,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                  children: List.generate(
+                      demoRecommendation.length,
+                      (index) => Padding(
+                            padding:
+                                const EdgeInsets.only(right: defaultPadding),
+                            child: RecommendationCard(recomendation: demoRecommendation[index],),
+                          ))),
+            )
+          ],
+        ),
+      )
     ]);
   }
 }
 
+class RecommendationCard extends StatelessWidget {
+  const RecommendationCard({
+    super.key, required this.recomendation,
+  });
+  final Recomendation recomendation;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      padding: EdgeInsets.all(defaultPadding),
+      color: secondaryColor,
+      child: Column(children: [
+        Text(
+          recomendation.name!,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        Text(recomendation.source!),
+        SizedBox(
+          height: defaultPadding,
+        ),
+        Text(
+          recomendation.text!,
+          style: TextStyle(height: 1.5),
+          maxLines: 4,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/recomandation.dart
@@ -0,0 +1,33 @@
+class Recomendation {
+  final String? name, source, text;
+
+  Recomendation({required this.name, required this.source, required this.text});
+}
+
+List<Recomendation> demoRecommendation = [
+    Recomendation(
+        name: "Md. Sifatullah",
+        source: "Facebook",
+        text: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus.",
+    ),
+    Recomendation(
+        name: "Md. Sifatullah",
+        source: "Instagram",
+        text: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus.",
+    ),
+    Recomendation(
+        name: "Md. Sifatullah",
+        source: "Linkedin",
+        text: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus.",
+    ),
+    Recomendation(
+        name: "Md. Sifatullah",
+        source: "Twitter",
+        text: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus.",
+    ),
+    Recomendation(
+        name: "Md. Sifatullah",
+        source: "Vmeo",
+        text: "The Covid-19 app is designed to help users stay informed and up to date with the latest news and information about the virus. And we also add a section for Q&A to know more about this virus.",
+    )
+];
